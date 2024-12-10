13a419c967c70463ba156798fa48bee4f0690b03
JANAPALA LOHITH KUMAR <127912459+lohithkumar-j@users.noreply.github.com>
2024-12-10 20:51:59 +0530

    Add files via upload

---
+++ Online_Art_Gallery-2/src/main/java/Online_Art_Galleryy/OnlineArtGallery2Application.java
@@ -0,0 +1,13 @@
+package Online_Art_Galleryy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineArtGallery2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineArtGallery2Application.class, args);
+	}
+
+}
---
+++ Online_Art_Gallery-2/src/test/java/Online_Art_Galleryy/OnlineArtGallery2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Online_Art_Galleryy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineArtGallery2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
